1066f9854c07ddd39a327cc07d39c60c8597d331
Jason Wong <jwong175@hotmail.com>
2019-07-26 13:34:25 -0700

    Edited user schema to include required & unique fields

--- models/users.js
+++ models/users.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    name: String,
-    email: String,
+    username: {type: String, require: true, unique: true},
+    password: {type: String, require: true},
+    name: {type: String, require: true},
+    email: {type: String, require: true, unique: true},
     phone: String,
     location: String,
     dogs: [{
